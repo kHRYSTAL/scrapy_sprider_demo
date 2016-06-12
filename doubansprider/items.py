96b11bf00d2a96d05dea4e75ab4691ef45c1f824
kHRYSTAL <723526676@qq.com>
2016-06-12 19:58:58 +0800

    commit demo

---
+++ doubansprider/items.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class DoubanspriderItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    # 电影名称
+    movie_name = scrapy.Field()
+    # 导演
+    movie_director = scrapy.Field()
+    # 编剧
+    movie_writer = scrapy.Field()
+    # 演员
+    movie_roles = scrapy.Field()
+    # 语言
+    movie_language = scrapy.Field()
+    # 上映时间
+    movie_date = scrapy.Field()
+    # 时长
+    movie_long = scrapy.Field()
+    # 电影描述
+    movie_description = scrapy.Field()
+
+
+class MeizituItem(scrapy.Item):
+    url = scrapy.Field()
+    name = scrapy.Field()
+    tags = scrapy.Field()
+    image_urls = scrapy.Field()
+    images = scrapy.Field()
+
